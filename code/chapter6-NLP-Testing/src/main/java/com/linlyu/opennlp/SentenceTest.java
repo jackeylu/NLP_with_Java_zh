9cbd099a4ed90564655f85568a2aa3ce37658a8f
Jackey Lyu <lvlin.china@gmail.com>
2015-08-15 19:49:03 +0800

    sample code for NLP-Java classifier

---
+++ code/chapter6-NLP-Testing/src/main/java/com/linlyu/opennlp/SentenceTest.java
@@ -0,0 +1,26 @@
+package com.linlyu.opennlp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.InvalidFormatException;
+
+public class SentenceTest {
+	public static void SentenceDetect() throws InvalidFormatException, IOException {
+		String paragraph = "Hi. How are you? This is Mike.";
+
+		// always start with a model, a model is learned from training data
+		InputStream is = new FileInputStream("en-sent.bin");
+		SentenceModel model = new SentenceModel(is);
+		SentenceDetectorME sdetector = new SentenceDetectorME(model);
+
+		String sentences[] = sdetector.sentDetect(paragraph);
+
+		System.out.println(sentences[0]);
+		System.out.println(sentences[1]);
+		is.close();
+	}
+}
---
+++ code/chapter6-NLP-Testing/src/main/java/com/linlyu/opennlp/SentenceTestTest.java
@@ -0,0 +1,18 @@
+package com.linlyu.opennlp;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import opennlp.tools.util.InvalidFormatException;
+
+public class SentenceTestTest {
+
+	@Test
+	public void testSentenceDetect() throws InvalidFormatException, IOException {
+		SentenceTest.SentenceDetect();
+	}
+
+}
