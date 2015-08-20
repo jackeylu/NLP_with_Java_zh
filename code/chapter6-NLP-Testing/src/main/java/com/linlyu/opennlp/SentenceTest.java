d21435c246afcbc3f09626d96ace3083ae1f878c
Jackey Lyu <lvlin.china@gmail.com>
2015-08-20 23:02:36 +0800

    update code on training model by training data

--- code/chapter6-NLP-Testing/src/main/java/com/linlyu/opennlp/SentenceTest.java
+++ code/chapter6-NLP-Testing/src/main/java/com/linlyu/opennlp/SentenceTest.java
@@ -1,19 +1,30 @@
 package com.linlyu.opennlp;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import opennlp.tools.doccat.DoccatFactory;
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.doccat.DocumentSample;
+import opennlp.tools.doccat.DocumentSampleStream;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.MarkableFileInputStreamFactory;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.TrainingParameters;
 
 public class SentenceTest {
-	public static void SentenceDetect() throws InvalidFormatException, IOException {
+	public static void SentenceDetect(String sent_model) throws InvalidFormatException, IOException {
 		String paragraph = "Hi. How are you? This is Mike.";
 
 		// always start with a model, a model is learned from training data
-		InputStream is = new FileInputStream("src/resource/en-sent.bin");
+		InputStream is = new FileInputStream(sent_model);
 		SentenceModel model = new SentenceModel(is);
 		is.close();
 		SentenceDetectorME sdetector = new SentenceDetectorME(model);
@@ -22,5 +33,20 @@ public class SentenceTest {
 
 		System.out.println(sentences[0]);
 		System.out.println(sentences[1]);		
+	}
+	
+	public static void  train(String file_train, String file_model) throws IOException {
+		DoccatModel model = null;
+		ObjectStream<String> lineStream =
+				new PlainTextByLineStream(new MarkableFileInputStreamFactory(
+						new File(file_train)), "UTF-8");
+		ObjectStream<DocumentSample> sampleStream =
+				new DocumentSampleStream(lineStream);
+
+		TrainingParameters param = TrainingParameters.defaultParams();
+		DoccatFactory factory = new DoccatFactory();
+		model = DocumentCategorizerME.train("en", sampleStream,param,factory);
+
+		model.serialize(new FileOutputStream(file_model));
 	}
 }
--- code/chapter6-NLP-Testing/src/test/java/com/linlyu/opennlp/SentenceTestTest.java
+++ code/chapter6-NLP-Testing/src/test/java/com/linlyu/opennlp/SentenceTestTest.java
@@ -1,7 +1,5 @@
 package com.linlyu.opennlp;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
 import org.junit.Test;
@@ -12,7 +10,11 @@ public class SentenceTestTest {
 
 	@Test
 	public void testSentenceDetect() throws InvalidFormatException, IOException {
-		SentenceTest.SentenceDetect();
+		SentenceTest.SentenceDetect("target/test-classes/en-sent.bin");
 	}
 
+	@Test
+	public void testTrain() throws IOException {
+		SentenceTest.train("sten-animal.train", "en_animal.model");
+	}
 }
