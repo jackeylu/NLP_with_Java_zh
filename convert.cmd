9296831f7d678c38362c4c2a2dfb24ff8f8d1204
Jackey Lyu <lvlin.china@gmail.com>
2015-08-22 16:58:26 +0800

    add docx pandoc command

--- convert.cmd
+++ convert.cmd
@@ -1,1 +1,3 @@
 pandoc  -S chapter6\chapter6.md -o chapter6.pdf --latex-engine=xelatex --template=template\pm-template.latex --variable mainfont="FandolHei"  
+
+pandoc -t docx  -S chapter6\chapter6.md -o chapter6.docx  
