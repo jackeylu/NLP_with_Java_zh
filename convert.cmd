e752e4e3f587e5142a06b0af6a028349cdab01e6
Jackey Lyu <lvlin.china@gmail.com>
2015-08-30 15:08:29 +0800

    just convert to docx, assp.

--- convert.cmd
+++ convert.cmd
@@ -1,3 +1,7 @@
-pandoc  -S chapter6\chapter6.md -o chapter6.pdf --latex-engine=xelatex --template=template\pm-template.latex --variable mainfont="FandolHei"  
+@REM pandoc  -S chapter6\chapter6.md -o chapter6.pdf --latex-engine=xelatex --template=template\pm-template.latex --variable mainfont="FandolHei"  
 
+@REM pandoc -t docx  -S chapter6\chapter6.md -o chapter6.docx  
 pandoc -t docx  -S chapter6\chapter6.md -o chapter6.docx  
+start chapter6.docx
+
+
